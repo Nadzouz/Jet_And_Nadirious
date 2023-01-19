f04fc3bf19b0e82ff4a54ae18d9a9481860df138
Nadzouz <119844377+Nadzouz@users.noreply.github.com>
2023-01-19 20:27:35 +0100

    committed all the project

---
+++ Nadzouz Figher jet/Nadzouz Figher jet/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿
+namespace Nadzouz_Figher_jet
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.enemyOne = new System.Windows.Forms.PictureBox();
+            this.enemyTwo = new System.Windows.Forms.PictureBox();
+            this.enemyThree = new System.Windows.Forms.PictureBox();
+            this.bullet = new System.Windows.Forms.PictureBox();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.txtScore = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.enemyOne)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemyTwo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemyThree)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bullet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // enemyOne
+            // 
+            this.enemyOne.Image = global::Nadzouz_Figher_jet.Properties.Resources.enemy;
+            this.enemyOne.Location = new System.Drawing.Point(81, 95);
+            this.enemyOne.Name = "enemyOne";
+            this.enemyOne.Size = new System.Drawing.Size(100, 85);
+            this.enemyOne.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.enemyOne.TabIndex = 0;
+            this.enemyOne.TabStop = false;
+            // 
+            // enemyTwo
+            // 
+            this.enemyTwo.Image = global::Nadzouz_Figher_jet.Properties.Resources.enemy;
+            this.enemyTwo.Location = new System.Drawing.Point(342, 95);
+            this.enemyTwo.Name = "enemyTwo";
+            this.enemyTwo.Size = new System.Drawing.Size(100, 85);
+            this.enemyTwo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.enemyTwo.TabIndex = 0;
+            this.enemyTwo.TabStop = false;
+            // 
+            // enemyThree
+            // 
+            this.enemyThree.Image = global::Nadzouz_Figher_jet.Properties.Resources.enemy;
+            this.enemyThree.Location = new System.Drawing.Point(643, 95);
+            this.enemyThree.Name = "enemyThree";
+            this.enemyThree.Size = new System.Drawing.Size(100, 85);
+            this.enemyThree.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.enemyThree.TabIndex = 0;
+            this.enemyThree.TabStop = false;
+            // 
+            // bullet
+            // 
+            this.bullet.Image = global::Nadzouz_Figher_jet.Properties.Resources.bullet;
+            this.bullet.Location = new System.Drawing.Point(394, 534);
+            this.bullet.Name = "bullet";
+            this.bullet.Size = new System.Drawing.Size(7, 27);
+            this.bullet.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.bullet.TabIndex = 0;
+            this.bullet.TabStop = false;
+            // 
+            // player
+            // 
+            this.player.Image = global::Nadzouz_Figher_jet.Properties.Resources.player;
+            this.player.Location = new System.Drawing.Point(380, 660);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(110, 98);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            // 
+            // txtScore
+            // 
+            this.txtScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtScore.Location = new System.Drawing.Point(1, 333);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(799, 164);
+            this.txtScore.TabIndex = 1;
+            this.txtScore.Text = "0";
+            this.txtScore.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.mainGameTimerEvent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 853);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.bullet);
+            this.Controls.Add(this.enemyThree);
+            this.Controls.Add(this.enemyTwo);
+            this.Controls.Add(this.enemyOne);
+            this.Controls.Add(this.txtScore);
+            this.Name = "Form1";
+            this.Text = "Jet & Nadirious";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.enemyOne)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemyTwo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemyThree)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bullet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox enemyOne;
+        private System.Windows.Forms.PictureBox enemyTwo;
+        private System.Windows.Forms.PictureBox enemyThree;
+        private System.Windows.Forms.PictureBox bullet;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Label txtScore;
+        private System.Windows.Forms.Timer gameTimer;
+    }
+}
+
